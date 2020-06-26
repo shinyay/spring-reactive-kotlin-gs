1d984c6c33ec2a4418def453afd9e3caad5e518f
shinyay <shinya.com@gmail.com>
2020-06-26 21:34:55 +0900

    Add Reactive Relational Database Connectivity H2

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("io.r2dbc:r2dbc-h2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
