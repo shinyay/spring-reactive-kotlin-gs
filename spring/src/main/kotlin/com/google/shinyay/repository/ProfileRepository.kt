3b7ac611f11cc3c37ea368db3cfeb57c012c34f9
shinyay <shinya.com@gmail.com>
2020-06-27 21:14:13 +0900

    Add repository interface extended ReactiveCrudRepository for Profile entity

--- spring/src/main/kotlin/com/google/shinyay/repository/ProfileRepository.kt
+++ spring/src/main/kotlin/com/google/shinyay/repository/ProfileRepository.kt
@@ -1,4 +1,8 @@
 package com.google.shinyay.repository
 
-class ProfileRepository {
-}
+import com.google.shinyay.model.Profile
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProfileRepository : ReactiveCrudRepository<Profile, Long>
