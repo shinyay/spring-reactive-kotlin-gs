99d666d424c3999c932043d67f3e1a29722e253a
shinyay <shinya.com@gmail.com>
2020-06-27 20:21:46 +0900

    Modify syntax

--- spring/src/main/kotlin/com/google/shinyay/model/ProfileRecord.kt
+++ spring/src/main/kotlin/com/google/shinyay/model/ProfileRecord.kt
@@ -1,4 +1,13 @@
 package com.google.shinyay.model
 
-class ProfileRecord {
-}
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDate
+
+@Table
+data class ProfileRecord(@Id var id: Long?,
+                         var profileId: Long?,
+                         var temperature: Double,
+                         var bloodPressure: Double,
+                         var heartRate: Double,
+                         var date: LocalDate)
