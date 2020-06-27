9230df2b17435de21013700badf79931c85d2b5e
shinyay <shinya.com@gmail.com>
2020-06-27 21:35:37 +0900

    Add status entity

---
+++ spring/src/main/kotlin/com/google/shinyay/model/Status.kt
@@ -0,0 +1,7 @@
+package com.google.shinyay.model
+
+data class Status(var cnt: Int,
+                  var temperature: Double,
+                  var bloodPressure: Double,
+                  var heartRate: Double) {
+}
