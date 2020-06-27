41449a840283f072729d0e0cf7d8d4488fb7bdaa
shinyay <shinya.com@gmail.com>
2020-06-27 20:59:16 +0900

    Add database schema creation

--- spring/src/main/kotlin/com/google/shinyay/config/DBConfiguration.kt
+++ spring/src/main/kotlin/com/google/shinyay/config/DBConfiguration.kt
@@ -1,7 +1,30 @@
 package com.google.shinyay.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.core.DatabaseClient
 
 @Configuration
-class DBConfiguration {
+class DBConfiguration(client: DatabaseClient) {
+    init {
+        client.execute {
+            """
+            CREATE TABLE IF NOT EXISTS profile (
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(20) NOT NULL,
+                last_name VARCHAR(20) NOT NULL,
+                birth_date DATE NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS health_record(
+                id SERIAL PRIMARY KEY,
+                profile_id LONG NOT NULL,
+                temperature DECIMAL NOT NULL,
+                blood_pressure DECIMAL NOT NULL,
+                heart_rate DECIMAL,
+                date DATE NOT NULL
+            );
+            """
+        }.also {
+            it.then().subscribe()
+        }
+    }
 }
