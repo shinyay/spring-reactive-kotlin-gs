3838131495fcfe4238df7cd17c5e84af460d77bb
shinyay <shinya.com@gmail.com>
2020-06-28 09:14:40 +0900

    Add repository for Person entity

--- spring/src/main/kotlin/com/google/shinyay/repository/PersonRepository.kt
+++ spring/src/main/kotlin/com/google/shinyay/repository/PersonRepository.kt
@@ -1,4 +1,15 @@
 package com.google.shinyay.repository
 
-class PersonRepository {
+import com.google.shinyay.model.Person
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
+
+interface PersonRepository : ReactiveCrudRepository<Person, Int> {
+
+    @Query("select id, name, age from person where name = :name")
+    fun findAllByName(name: String): Flux<Person>
+
+    @Query("select * from player where age = :age")
+    fun findByAge(age: Int)
 }
