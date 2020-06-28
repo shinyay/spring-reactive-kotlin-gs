da669bb896a933ebb73b3c33bf673ee194579c03
shinyay <shinya.com@gmail.com>
2020-06-28 11:39:01 +0900

    Add find function by Last Name

--- spring/src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ spring/src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -1,15 +1,19 @@
 package com.google.shinyay.repository
 
-import com.google.shinyay.model.Person
+import com.google.shinyay.model.Employee
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
-interface PersonRepository : ReactiveCrudRepository<Person, Int> {
+interface EmployeeRepository : ReactiveCrudRepository<Employee, Long> {
 
-    @Query("SELECT id, name, age FROM person WHERE name = :name")
-    fun findAllByName(name: String): Flux<Person>
+    @Query("select e.* from Employee e where e.last_name = :lastName")
+    fun findEmployeeByLastName(lastName: String): Mono<Employee>
 
-    @Query("SELECT * FROM player WHERE age = :age")
-    fun findByAge(age: Int)
+//    @Query("SELECT id, name, age FROM person WHERE name = :name")
+//    fun findAllByName(name: String): Flux<Employee>
+//
+//    @Query("SELECT * FROM player WHERE age = :age")
+//    fun findByAge(age: Int)
 }
