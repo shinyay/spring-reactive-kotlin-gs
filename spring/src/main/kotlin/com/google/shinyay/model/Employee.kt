b2e0a90e7cf695437f15350b1849b7fe153084aa
shinyay <shinya.com@gmail.com>
2020-06-29 00:02:02 +0900

    Add secondry constructor for two parameters

--- spring/src/main/kotlin/com/google/shinyay/model/Employee.kt
+++ spring/src/main/kotlin/com/google/shinyay/model/Employee.kt
@@ -7,5 +7,5 @@ import org.springframework.data.relational.core.mapping.Table
 @Table("employee")
 data class Employee(@Id val id: Long?,
                     @Column("first_name") val firstName: String,
-                    @Column("last_name")val latName: String
-)
+                    @Column("last_name")val lastName: String
+){constructor(firstName: String, lastName: String) : this(id = null, firstName = firstName, lastName = lastName) }
