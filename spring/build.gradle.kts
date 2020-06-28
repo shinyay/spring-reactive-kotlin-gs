9e21dc61e4b63e506e80b813579934a2ee655bd2
shinyay <shinya.com@gmail.com>
2020-06-28 09:15:20 +0900

    Add kotlin plugin

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
+	kotlin("plugin.allopen") version "1.3.72"
+	kotlin("plugin.noarg") version "1.3.72"
+	kotlin("plugin.jpa") version "1.3.72"
 }
 
 group = "com.google.shinyay"
