ae3d5127ce806394e7de85d10e430705579d1b1c
shinyay <shinya.com@gmail.com>
2020-07-01 22:54:49 +0900

    Add select by id function

--- spring/src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ spring/src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -10,6 +10,9 @@ interface EmployeeRepository : R2dbcRepository<Employee, Long> {
     @Query("select e.* from Employee e where e.last_name = :lastName")
     fun findEmployeeByLastName(lastName: String): Mono<Employee>
 
+    @Query("select e.* from Employee e where e.id = :idsrc/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt")
+    fun findEmployeeById(id: Long): Mono<Employee>
+
 //    @Query("SELECT id, name, age FROM person WHERE name = :name")
 //    fun findAllByName(name: String): Flux<Employee>
 //
